e64d92ec431e20ecd3c5bb5bc7b100593e1d4255
herlanderbento <herlanderbento19@gmail.com>
2021-12-05 02:41:15 +0100

    create model user

---
+++ src/modules/users/model/User.ts
@@ -0,0 +1,18 @@
+import { v4 as uuid } from "uuid";
+
+class User {
+  id?: string;
+  name: string;
+  admin: string;
+  email: string;
+  created_at: Date;
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { User };
